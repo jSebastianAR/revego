9df2c12d93e151c776d4ca49588c9aee248d252d
Jair Sebastián Acosta Rosales <jSebastian_AR@outlook.com>
2021-04-04 17:36:43 -0500

    Eliminada instrucción de creación de superusuario

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,17 +5,6 @@ python pumps/manage.py makemigrations --noinput
 echo "Migrate..."
 python pumps/manage.py migrate --noinput
 
-#Try catch if user already exist
-{
-    echo "Creating superuser: $DJANGO_SUPERUSER_USERNAME"
-    python pumps/manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL
-} || {
-    echo "Superuser $DJANGO_SUPERUSER_USERNAME already exists"
-}
-
 echo "Starting server..."
 python pumps/manage.py runserver 0.0.0.0:8080 --insecure
 # and add this at the end
